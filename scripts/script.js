5d9b9ae90382952fa966a43dde5bbdaf4e81232e
Magomed Akhmaev <m.axmaev@yandex.ru>
2026-02-16 17:59:48 +0300

    Buttons for changing the theme on the site have been added, work has begun on theme-changing functions

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const darkThemeBtn = document.querySelector('.dark-theme');
+const lightThemeBtn = document.querySelector('.ligh-theme');
+const formContainer = document.querySelector('.form');
+const formHeader = document.querySelector('.form__header');
+const formInput = document.querySelectorAll('.form__input');
+
+darkThemeBtn.onclick = () =>{
+
+}
